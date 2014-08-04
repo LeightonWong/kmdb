6294f520547bcd59e647f74cc9d1e2442f508b80
WangLei <leith.wong@gmail.com>
2014-08-04 20:02:25 +0800

    temp submit

--- protocol.go
+++ protocol.go
@@ -2,9 +2,20 @@ package kmdb
 
 import (
 	"bytes"
+	"encoding/binary"
 	"fmt"
 	"io"
 	"strconv"
+)
+
+const (
+	ASTERISK_BYTE byte = '*'
+	DOLLAR_BYTE   byte = '$'
+	PLUS_BYTE     byte = '+'
+	MINUS_BYTE    byte = '-'
+	COLON_BYTE    byte = ':'
+	CR_BYTE       byte = '\r'
+	LF_BYTE       byte = '\n'
 )
 
 type Operation int
@@ -19,6 +30,12 @@ const (
 type Command struct {
 	op   Operation
 	args []byte
+	sync bool
+}
+
+type CommandBatch struct {
+	comms []Command
+	sync  bool
 }
 
 type StatusReply string
@@ -32,4 +49,16 @@ type BulkReply []byte
 type MultiBulkReply struct {
 	size    int
 	replies []BulkReply
+}
+
+func DecodeCommand(buf []byte, comm *Command) error {
+	if len(buf) == 0 {
+		return Error("Empty data")
+	}
+	if buf[0] != ASTERISK_BYTE {
+		return Error("Wrong Command Format " + string(buf))
+	}
+
+	oplen, err := binary.ReadUvarint(bytes.NewBuffer(buf[1:1]))
+
 }
